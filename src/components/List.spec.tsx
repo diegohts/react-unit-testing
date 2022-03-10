575f24f8d58c2b276f06443f47b48060d69802a7
Diego Henrique <diegohrp20@gmail.com>
2022-03-10 02:33:33 -0300

    Finalizing the test project to add or remove elements from the list.

--- src/components/List.spec.tsx
+++ src/components/List.spec.tsx
@@ -1,18 +1,25 @@
 import { render, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from './App';
+import { unmountComponentAtNode } from 'react-dom';
+import List from './List';
 
-describe('App Component', () => {
+describe('List Component', () => {
     it('should render list items', () => {
-        const { getByText } = render(<App />);
+        const { getByText, rerender, queryByText, unmount  } = render(<List initialItems={['Diego', 'Fernando', 'Henrique']}/>);
 
         expect(getByText('Diego')).toBeInTheDocument();
         expect(getByText('Fernando')).toBeInTheDocument();
         expect(getByText('Henrique')).toBeInTheDocument();
+
+        unmount();
+        rerender(<List initialItems={['Julia']}/>);
+
+        expect(getByText('Julia')).toBeInTheDocument();
+        expect(queryByText('Henrique')).not.toBeInTheDocument();
     });
 
     it('should be able to add new item to the list', async () => {
-        const { getByText, getByPlaceholderText } = render(<App />);
+        const { getByText, getByPlaceholderText } = render(<List initialItems={[]}/>);
 
         const inputElement = getByPlaceholderText('Novo item');
         const addButton = getByText('Adicionar');
@@ -26,7 +33,7 @@ describe('App Component', () => {
     });
 
     it('should be able to add remove item from the list', async () => {
-        const { getByText, getAllByText } = render(<App />);
+        const { getByText, getAllByText } = render(<List initialItems={['Diego']}/>);
 
         const removeButtons = getAllByText('Remover');
 
